d24648862e03d70980a620f38aabc4b387f102f7
Maxim <11011111@inbox.ru>
2020-10-04 17:13:10 +0300

    enter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
+
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dislogs";
